bb4bab39842f36594ba397f018df8db5862214de
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-04-29 11:11:06 +0300

    car lists readability update

--- lesson 8 adv/Program.cs
+++ lesson 8 adv/Program.cs
@@ -18,13 +18,15 @@ namespace lesson_8_adv
             //Console.WriteLine(masina.Drive(ref kuras));
             //Console.WriteLine(masina.Drive(ref kuras));
 
-            var bmwCars = new List<BmwCar> { new BmwCar(true, "model2"), new BmwCar(true, "model3"), new BmwCar(true, "model1") };
+            var bmwCars = new List<BmwCar> 
+            {   
+                new BmwCar(true, "model2"),
+                new BmwCar(true, "model3"), 
+                new BmwCar(true, "model1") 
+            };
             var carComparer = new BmwCarComparer();
             bmwCars.Sort(carComparer);
-            foreach (var c in bmwCars)
-            {
-                Console.WriteLine(c.Model);
-            }
+            foreach (var c in bmwCars) Console.WriteLine(c.Model);
             //var bmw = new BmwCar(false, "X5");
             //Console.WriteLine($"BMW: {bmw.Drive(ref kuras)}");
             //Console.WriteLine(bmw.Drive(ref kuras));
@@ -33,13 +35,15 @@ namespace lesson_8_adv
             //Console.WriteLine(bmw.Drive(ref kuras));
             //Console.WriteLine(bmw.Drive(ref kuras));
 
-            var audiCars = new List<AudiCar> { new AudiCar(false, "model20"), new AudiCar(false, "model30"), new AudiCar(false, "model10") };
+            var audiCars = new List<AudiCar>
+            {
+                new AudiCar(false, "model20"),
+                new AudiCar(false, "model30"),
+                new AudiCar(false, "model10")
+            };
             var carComparer1 = new AudiCarComparer();
             audiCars.Sort(carComparer1);
-            foreach (var c in audiCars)
-            {
-                Console.WriteLine(c.Model);
-            }
+            foreach (var c in audiCars) Console.WriteLine(c.Model);
             //var audi = new AudiCar(false, "C4");
             //Console.WriteLine($"Audi model: {audi.Model}");
             //Console.WriteLine($"Is Quattro: {audi.IsQuattro}");
